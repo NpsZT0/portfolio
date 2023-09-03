1af9a29eb38a26002e0f5ef8e93493bf9cf0c0a7
NpsZT0 <hearorsimtang@gmail.com>
2023-09-04 02:07:23 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false)
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -28,15 +29,39 @@ const Navbar = () => {
             <li
               key={link.id}
               className={`${active === link.title
-                  ? "text-white"
-                  : "text-secondary"
+                ? "text-white"
+                : "text-secondary"
                 } hover:text-white text-[18px]
               font-medium cursor-pointer`}
             >
-              <a href={`#${link.id}`} onClick={()=>setActive(link.title)}>{link.title}</a>
+              <a href={`#${link.id}`} onClick={() => setActive(link.title)}>{link.title}</a>
             </li>
           ))}
         </ul>
+        <div className='flex items-center justify-end flex-1 sm:hidden'>
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className='flex flex-col items-start justify-end gap-4 list-none '>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${active === link.title
+                    ? "text-white"
+                    : "text-secondary"
+                    } hover:text-white text-[18px]
+              font-medium cursor-pointer`}
+                >
+                  <a href={`#${link.id}`} onClick={() => setActive(link.title)}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
